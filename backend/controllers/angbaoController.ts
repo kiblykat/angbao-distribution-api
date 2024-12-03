ee9ee3d1a4dd1ba101c6aa951630fc61e1dc1a22
kiblykat <izzatfadzlon@gmail.com>
2024-12-03 10:49:42 +0800

    angbaoController: update distributeAngbaos to accept userArray and return allocations

--- backend/controllers/angbaoController.ts
+++ backend/controllers/angbaoController.ts
@@ -6,10 +6,26 @@ export const distributeAngbaos = async (
   res: express.Response
 ) => {
   try {
-    let { totAmount, numUsers } = req.body;
-    let response = allocateRandomAmounts(totAmount, numUsers);
-    res.status(200).json({ res: response });
+    let hash: { [key: string]: number } = {}; //take note of this definition*
+
+    //receive request from user
+    let { totAmount, userArray } = req.body;
+    totAmount = parseFloat(totAmount);
+
+    const userArrayParsed: string[] = JSON.parse(userArray);
+    console.log(totAmount, userArrayParsed);
+
+    for (let user of userArrayParsed) {
+      hash[user] = 0;
+    }
+    let numUsers = userArrayParsed.length;
+    let allocationArray: number[] = allocateRandomAmounts(totAmount, numUsers); //get allocationArray back from service
+    for (let i = 0; i < allocationArray.length; i++) {
+      hash[userArrayParsed[i]] = allocationArray[i];
+    }
+    res.status(200).json({ res: hash });
   } catch (err) {
     res.status(500).json({ err: err });
+  } finally {
   }
 };
