d68772535434988cd168034f85df823d4d7bd019
kiblykat <izzatfadzlon@gmail.com>
2024-12-04 12:04:21 +0800

    test: mock userModel and add tests for /angbaos and /users routes

--- backend/server.test.ts
+++ backend/server.test.ts
@@ -14,6 +14,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Mock the userModel
+// this mocks 'const users = await userModel.find();' in userController
+jest.mock("./models/User", () => ({
+  userModel: {
+    find: jest.fn().mockResolvedValue([
+      { _id: "1", username: "John", balance: 10000 },
+      { _id: "2", username: "Jane", balance: 10000 },
+    ]),
+    findById: jest
+      .fn()
+      .mockResolvedValue([{ _id: "2", username: "Jane", balance: 10000 }]),
+  },
+  findByIdAndUpdate: jest.fn(),
+}));
+
 app.use("/angbaos", angbaoRouter);
 app.use("/users", userRouter);
 
@@ -26,5 +41,21 @@ describe("Server", () => {
   it("should respond with 404 for unknown routes", async () => {
     const response = await request(app).get("/unknown-route");
     expect(response.status).toBe(404);
+  });
+
+  it("should respond to /users route", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+    expect(response.body.users).toHaveLength(2);
+  }, 5000);
+
+  it("should respond to /angbaos route", async () => {
+    const response = await request(app).post("/angbaos/distribute").send({
+      currUserId: "674e7824a6cddab6818afe6f",
+      totAmountDollars: "10.23",
+      userArray:
+        '["674e7824a6cddab6818afe6f","674e79c0cba2edf55d536255","674e79c5cba2edf55d536257"]',
+    });
+    expect(response.status).not.toBe(404);
   });
 });
