e3c8062609977974e26d599a9abf2c5c63475c12
kiblykat <izzatfadzlon@gmail.com>
2024-12-05 11:12:15 +0800

    test: add getAllUsers unit test for userController

---
+++ backend/controllers/userController.test.ts
@@ -0,0 +1,50 @@
+import { userModel } from "../models/User";
+import { Request, Response } from "express";
+import request from "supertest";
+import express from "express";
+import userRouter from "../routes/userRouter";
+
+jest.mock("../models/User", () => ({
+  userModel: {
+    find: jest.fn(),
+    create: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+    findById: jest.fn(),
+  },
+}));
+
+const app = express();
+app.use(express.json());
+app.use("/users", userRouter);
+
+describe("userController", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should getAllUsers with status code 200", async () => {
+    (userModel.find as jest.Mock).mockResolvedValue({
+      _id: "674dcad282b19cbaadf64aa5",
+      username: "John",
+      balance: 5100,
+      __v: 0,
+    });
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      users: {
+        _id: "674dcad282b19cbaadf64aa5",
+        username: "John",
+        balance: 5100,
+        __v: 0,
+      },
+    });
+  });
+
+  it("should return an error with status code 500", async () => {
+    (userModel.find as jest.Mock).mockRejectedValue("Database Error");
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({ error: "Database Error" });
+  });
+});
